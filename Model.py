30164a8d318afcd044d87c4e5563bce40c9d2e71
Walid Babi <waledo9397@gmail.com>
2023-12-25 01:22:46 +0300

    model training

--- Model.py
+++ Model.py
@@ -1,8 +1,6 @@
 import numpy as np
-import numpy.ma as ma
 import pandas as pd
 import tensorflow as tf
-import openpyxl
 from tensorflow import keras
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
@@ -23,7 +21,6 @@ category_encoded = encoder.fit_transform(toy_features[["category"]])
 age_encoded = encoder.fit_transform(toy_features[["age"]])
 gender_encoded = encoder.fit_transform(toy_features[["gender"]])
 
-
 #Encode "child_age" and "child_gender" and "child_preferred_category features 
 child_age_encoded = encoder.fit_transform(child_features[["child_age"]])
 child_gender_encoded = encoder.fit_transform(child_features[["child_gender"]])
@@ -42,39 +39,50 @@ child_preferred_category_encoded_df = pd.DataFrame(child_preferred_category_enco
 
 child_features = pd.concat([child_age_encoded_df, child_gender_encoded_df, child_preferred_category_encoded_df, child_features.iloc[:, 3:]], axis=1)
 
-# # Save toy_features to Excel
-# toy_features.to_excel("processed_toy_features.xlsx", index=False)  # Set index=False to omit row indices
+toy_features.columns = toy_features.columns.astype(str)
+child_features.columns = child_features.columns.astype(str)
 
-# # Save child_features to Excel
-# child_features.to_excel("processed_child_features.xlsx", index=False)
+# Output preprocessed toy features to CSV
+toy_features.to_csv("toy_features_preprocessed.csv", index=False)
 
-# print("Data saved to Excel files successfully!")
+# Output preprocessed child features to CSV
+child_features.to_csv("child_features_preprocessed.csv", index=False)
 
 
-# Consider scaling or normalization if necessary
-scaler = StandardScaler()  # Or MinMaxScaler()
-toy_features = scaler.fit_transform(toy_features)
-child_features = scaler.fit_transform(child_features)
+# Split data into separate training sets for toys and child models
+X_train_toys, X_test_toys, y_train_toys, y_test_toys = train_test_split(toy_features, toy_features, test_size=0.2, random_state=42)  # Use toy_features for both input and output
+X_train_child, X_test_child, y_train_child, y_test_child = train_test_split(child_features, child_features, test_size=0.2, random_state=42)  # Use child_features for both input and output
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(toy_features, child_features, test_size=0.2, random_state=42)
-
-# Build and train a model (e.g., a neural network)
-model = keras.Sequential([
+toys_model = keras.Sequential([
+    keras.layers.Dense(128, activation='relu'),
     keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(64, activation='relu'),
-    keras.layers.Dense(4)  # Output layer matching number of features
+    keras.layers.Dense(46)  # Output layer matching the number of toy features
 ])
 
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(X_train, y_train, epochs=100, validation_data=(X_test, y_test))
+child_model = keras.Sequential([
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(37)  # Output layer matching the number of child features
+])
 
-# Use the trained model to make recommendations
-# ... (code for generating recommendations based on child's profile)
+# Train the toys model
+toys_model.compile(optimizer='adam', loss='mean_squared_error')
+toys_model.fit(X_train_toys, y_train_toys, epochs=100, validation_data=(X_test_toys, y_test_toys))
+
+# Train the child model
+child_model.compile(optimizer='adam', loss='mean_squared_error')
+child_model.fit(X_train_child, y_train_child, epochs=100, validation_data=(X_test_child, y_test_child))
+
+toy_vectors = toys_model.predict(toy_features)
+child_vector = child_model.predict(child_features)
+
+def dot_product(child_vector, toy_vector):
+    return np.dot(child_vector, toy_vector)
+
+similarity_scores = [dot_product(child_vector, toy_vector) for toy_vector in toy_vectors]
+
+top_recommendations = np.argsort(-similarity_scores)[:5]  # Top 5 recommendations
+
+recommended_toys = toys.iloc[top_recommendations]
 
 
-# print("First 5 rows of toys dataset:")
-# print(toys.head())
-
-# print("\nFirst 5 rows of children dataset:")
-# print(children.head())
