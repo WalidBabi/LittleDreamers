8032cc3cece39aef6b070160e1d5a06eb5552392
Walid Babi <waledo9397@gmail.com>
2024-02-23 22:51:09 +0300

    toy description json returned

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Toy;
+use App\Models\ToyDescription;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -17,11 +18,17 @@ class ProductController extends Controller
     public function show($id)
     {
         $product = Toy::find($id);
+
+        $toy_description = ToyDescription::find($id)->toArray();;
         
         if (!$product) {
             return response()->json(['message' => 'Product not found'], 404);
         }
-        
-        return response()->json($product);
+
+        // return response()->json($product);
+        return response()->json([
+            'product' => $product,
+            'description' => $toy_description 
+        ], 200);
     }
 }
