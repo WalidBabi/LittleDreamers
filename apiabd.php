4c87d5c69be6838308efa520a0a81fc1448a9a61
AbdAljabbar-Alshareef <155620504+AbdAljabbar-Alshareef@users.noreply.github.com>
2024-02-25 13:45:45 +0300

    Add files via upload

---
+++ apiabd.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PassportAuthController;
+use App\Http\Controllers\Api\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+ 
+Route::middleware('auth:api')->group(function () {
+Route::get('logout', [PassportAuthController::class, 'logout']);
+
+});
+
+//controller products
+Route::resource('products', productController::class);
